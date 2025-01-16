44904a8d6819b64d9b71a2c38bf416d38e0b0cfe
innovativeinstitution <innovativeinstitution@gmail.com>
2025-01-16 09:51:35 -0800

    infrastructure layer

--- AirlineBookingSystem/AirlineBookingSystem.Flights.Api/Program.cs
+++ AirlineBookingSystem/AirlineBookingSystem.Flights.Api/Program.cs
@@ -1,3 +1,8 @@
+using AirlineBookingSystem.Flights.Core.Repositories;
+using AirlineBookingSystem.Flights.Infrastructure.Repositories;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Application Services
+builder.Services.AddScoped<IFlightRepository, FlightRepository>();
+
+// Add Sql Connection
+builder.Services.AddScoped<IDbConnection>(sp =>
+    new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
--- AirlineBookingSystem/AirlineBookingSystem.Payments.Api/Program.cs
+++ AirlineBookingSystem/AirlineBookingSystem.Payments.Api/Program.cs
@@ -1,3 +1,8 @@
+using AirlineBookingSystem.Payments.Core.Repositories;
+using AirlineBookingSystem.Payments.Infrastructure.Repositories;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Application Services
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+
+// Add Sql Connection
+builder.Services.AddScoped<IDbConnection>(sp =>
+    new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
